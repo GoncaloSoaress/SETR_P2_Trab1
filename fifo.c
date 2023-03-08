47a94d52c5ee087d32b3064fe1e558521e4d80a9
GoncaloSoaress <goncalo.soares01@ua.pt>
2023-03-08 22:39:42 +0000

    Added FIFO Insert and Peep

--- fifo.c
+++ fifo.c
@@ -2,3 +2,16 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "fifo.h"
+
+void myFIFOInsert(struct MYFIFO *fifo, uint32_t val){
+    if (fifo->cnt == FIFO_SIZE){
+        myFIFORemove(fifo);
+    }
+    fifo->array[fifo->in_p].value = val;
+    fifo->in_p = (fifo->in_p + 1) % FIFO_SIZE;
+    fifo->cnt++;
+}
+
+uint32_t myFIFOPeep(struct MYFIFO *fifo){
+    return fifo->array[fifo->out_p].value;
+}
