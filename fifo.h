5cc2cf3a09f41b1305c27be759c12b01ffcda475
Young-Owl <goncalorodrigues@ua.pt>
2023-03-08 23:14:35 +0000

    Added Doxygen Documentation for the last commit functions.

--- fifo.h
+++ fifo.h
@@ -11,17 +11,44 @@ struct CONTENT {
     uint32_t value;
 };
 
+
+/**
+ * @brief This structure contains all the variables needed to implement a FIFO structure.
+ */
 struct MYFIFO {
-    uint32_t cnt;
-    uint32_t in_p;
-    uint32_t out_p;
-    CONTENT array[FIFO_SIZE];
+    uint32_t cnt;                   /**<Number of elements in the FIFO structure.*/
+    uint32_t in_p;                  /**<Pointer to the next position to insert an element in the FIFO structure.*/
+    uint32_t out_p;                 /**<Pointer to the next position to remove an element from the FIFO structure.*/
+    CONTENT array[FIFO_SIZE];       /**<Array of elements in the FIFO structure of size FIFO_SIZE. This array itself is a structure of the CONTENT type.*/
 };
 
+/**
+ * @brief This function initializes the FIFO structure, setting all the variables to 0.
+ * 
+ * @param fifo Pointer to the FIFO structure to be initialized.
+ */
 void myFIFOInit(struct MYFIFO *fifo);
+
+
 void myFIFOInsert(struct MYFIFO *fifo, uint32_t val);
+
+/**
+ * @brief This function removes the oldest element from the FIFO structure.
+ * 
+ * @param fifo Pointer to the FIFO structure.
+ * @return The value of the oldest element in the FIFO structure. (uint32_t)
+ */
 uint32_t myFIFORemove(struct MYFIFO *fifo);
+
+
 uint32_t myFIFOPeep(struct MYFIFO *fifo);
+
+/**
+ * @brief This function returns the number of elements in the FIFO structure.
+ * 
+ * @param fifo Pointer to the FIFO structure.
+ * @return The number of elements in the FIFO structure. (uint32_t)
+ */
 uint32_t myFIFOSize(struct MYFIFO *fifo);
 
 #endif
